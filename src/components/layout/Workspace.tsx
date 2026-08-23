cb6a94dfc351d0912bec6022038a1c2a1cdf4097
Killer88967 <Erniebug27@gmail.com>
2026-08-23 03:03:59 +0000

    added monaco

--- src/components/layout/Workspace.tsx
+++ src/components/layout/Workspace.tsx
@@ -1,7 +1,80 @@
+import Editor from "@monaco-editor/react";
+
 import type { OpenedFile } from "../../types/editor";
 
 interface WorkspaceProps {
   openedFile: OpenedFile | null;
+}
+
+function getLanguage(filename: string) {
+  const extension = filename.split(".").pop()?.toLowerCase();
+
+  switch (extension) {
+    case "ts":
+    case "tsx":
+      return "typescript";
+
+    case "js":
+    case "jsx":
+      return "javascript";
+
+    case "rs":
+      return "rust";
+
+    case "py":
+      return "python";
+
+    case "cs":
+      return "csharp";
+
+    case "c":
+      return "c";
+
+    case "cpp":
+    case "cc":
+    case "cxx":
+    case "hpp":
+    case "h":
+      return "cpp";
+
+    case "java":
+      return "java";
+
+    case "go":
+      return "go";
+
+    case "json":
+      return "json";
+
+    case "html":
+      return "html";
+
+    case "css":
+      return "css";
+
+    case "scss":
+      return "scss";
+
+    case "md":
+      return "markdown";
+
+    case "yaml":
+    case "yml":
+      return "yaml";
+
+    case "xml":
+      return "xml";
+
+    case "sql":
+      return "sql";
+
+    case "sh":
+    case "bash":
+      return "shell";
+
+    default:
+      return "plaintext";
+  }
 }
 
 function Workspace({ openedFile }: WorkspaceProps) {
@@ -22,9 +95,25 @@ function Workspace({ openedFile }: WorkspaceProps) {
         <div className="editor-tab active">{openedFile.name}</div>
       </div>
 
-      <pre className="plain-editor">
-        <code>{openedFile.content}</code>
-      </pre>
+      <div className="monaco-container">
+        <Editor
+          path={openedFile.path}
+          language={getLanguage(openedFile.name)}
+          value={openedFile.content}
+          theme="vs-dark"
+          options={{
+            automaticLayout: true,
+            fontSize: 13,
+            minimap: {
+              enabled: true,
+            },
+            scrollBeyondLastLine: false,
+            smoothScrolling: true,
+            tabSize: 2,
+            wordWrap: "off",
+          }}
+        />
+      </div>
     </main>
   );
 }
